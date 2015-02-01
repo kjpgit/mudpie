cf4695a512f1f1380796de20f01fb15da48629a7
kjpgit <kjpgit@users.noreply.github.com>
2015-02-01 08:10:11 -0600

    add html_element_escape

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(unstable)]
 pub use webserver::{WebServer, WebRequest, WebResponse};
 pub use webserver::{PageFunction};
+pub use utils::escape::html_element_escape;
 mod utils;
 mod webserver;
